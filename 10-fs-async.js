a6f9a1e8ade39f65ce0973f5f4569d8047ba22a6
Aman Kumar <113698292+amannkumar@users.noreply.github.com>
2024-01-29 22:44:12 -0500

    moearning more of node js

---
+++ 10-fs-async.js
@@ -0,0 +1,9 @@
+const {readFile,writeFilw} = require('fs')
+
+readFile('./content/first.txt','utf8',(err,result) =>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+}) 
--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 //Calling mind grenade in main file
 //require('./6-mind-grenade')
 
-const {readFile,writeFilw} = require('fs')
-
-readFile('./content/first.txt','utf8',(err,result) =>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log(result)
-}) 
